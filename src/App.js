f99304a2072e61236fc4b7e59ad2bd58df954acf
ShreyasiC(INT002) <53420894+shreyasi-chatterjee@users.noreply.github.com>
2023-12-14 13:02:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import classes from './App.module.css';
+import ProductData from './ProductData';
+import ProductDetail from './ProductDetail/ProductDetail';
+import ProductPrev from './ProductPreview/ProductPrev';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    productData: ProductData,
+    currentPrevImgPos: 0,
+    currentFeatOpt: 0
+  }
+
+  onClickColorOpt = (pos) => {
+    this.setState({currentPrevImgPos: pos});
+  }
+  onClickFeatOpt = (pos) => {
+    this.setState({currentFeatOpt: pos});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <nav className={classes.Topbar}>
+            <img src={window.location.origin + '/amazonLogo.png'} alt="logo" />
+          </nav>
+          <main>
+            <div className={classes.Productcont}>
+              <div className={classes.ProductPrev}>
+              <ProductPrev currentPrevImage={this.state.productData.colorOptions[this.state.currentPrevImgPos].imageUrl}
+              currentFeatOpt={this.state.currentFeatOpt} />
+              </div>
+                <ProductDetail data={this.state.productData} onClickColorOpt={this.onClickColorOpt} 
+                currentPrevImgPos={this.state.currentPrevImgPos} onClickFeatOpt={this.onClickFeatOpt}
+                currentFeatOpt={this.state.currentFeatOpt}/>
+            </div>
+          </main>
+  
+        </header>
+      </div>
+    );
+  }
+  
+};
 
 export default App;
